c3d6042ad944bf1058783eb17e220279ec1986de
levi-katarok <levi@katarok.ca>
2025-01-27 11:22:33 -0500

    testing commit message

--- commitry_cli/git_utils.py
+++ commitry_cli/git_utils.py
@@ -5,3 +5,7 @@ def get_staged_diff():
     # Alternatively, you can use GitPython
     result = subprocess.run(["git", "diff", "--staged"], capture_output=True, text=True)
     return result.stdout
+
+def get_staged_diff_gitpython():
+    light_on = True
+    return light_on
--- commitry_cli/test_deepseek.py
+++ commitry_cli/test_deepseek.py
@@ -17,11 +17,12 @@ client = openai.Client(
 
 
 def generate_commit_message(diff):
+    system_prompt = "You are a helpful assistant that generates concise user git commit messages for git commits. You will recieve git diffs and you will return a concise commit message. Your job is to summarize the changes in the diff into a concise commit message."
+    import pdb; pdb.set_trace()
     response = client.chat.completions.create(
         model="deepseek-r1:1.5b",
-        messages=[{"role": "user", "content": diff}],
+        messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": diff}],
         stream=True
     )
     for chunk in response:
         print(chunk.choices[0].delta.content, end="", flush=True)
-    return response.choices[0].message.content
